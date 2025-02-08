a1bdb14fed8ae16ebefe0217498a327269da7f27
Anderson Blas <andi_1516@hotmail.com>
2025-02-08 01:15:14 -0500

    Create Service layout

---
+++ app/Infrastructure/Services/AppDataService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Infrastructure\Services;
+
+use Illuminate\Support\Facades\File;
+
+class AppDataService
+{
+    public function getAppData(string $appId): ?array
+    {
+        $apps = json_decode(File::get(storage_path('app/app.json')), true);
+        return collect($apps)->firstWhere('id', $appId);
+    }
+
+    public function getDeveloperData(string $developerId): ?array
+    {
+        $developers = json_decode(File::get(storage_path('app/developer.json')), true);
+        return collect($developers)->firstWhere('id', $developerId);
+    }
+}
